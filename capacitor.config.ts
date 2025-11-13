0cb6d5869e3b87cad76aeef08924de7629ea465f
instancecom <instance.com@gmail.com>
2025-11-13 10:49:49 -0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'app.lovable.2c86ecd5dc17498bb3b1024e12a6e5ff',
-  appName: 'tuntuntunbkp-01',
+  appName: 'IACM Manager',
   webDir: 'dist',
   server: {
     url: 'https://2c86ecd5-dc17-498b-b3b1-024e12a6e5ff.lovableproject.com?forceHideBadge=true',
